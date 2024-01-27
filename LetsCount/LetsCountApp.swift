873120d6ee1d4f5f3cb08abfdbb027b55cc48f40
Hanna Christensson <145654643+HannaChristensson@users.noreply.github.com>
2024-01-27 19:11:43 +0100

    Initial Commit

---
+++ LetsCount/LetsCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsCountApp.swift
+//  LetsCount
+//
+//  Created by Hanna Christensson on 2024-01-27.
+//
+
+import SwiftUI
+
+@main
+struct LetsCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
